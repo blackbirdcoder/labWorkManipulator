69ebcc8990b643239feaf9930be0b961d412fd76
Eugene-TechnoNinja <technoninja@ukr.net>
2025-11-09 11:46:23 +0200

    added full gui style

--- src/ui.cpp
+++ src/ui.cpp
@@ -3,9 +3,9 @@
 #define RAYGUI_IMPLEMENTATION
 #include <raygui.h>
 #include "ui.h"
-#include "style_amber.h"
 // clang-format on
-GUI::GUI() { GuiLoadStyleAmber(); }
+
+GUI::GUI() { GuiLoadStyle("style_amber.rgs"); }
 
 void GUI::StateSwitchButton(float btnPosX, bool *statePtr, const char *title) {
   if (GuiButton((Rectangle){btnPosX, 20, 110, 30}, title)) {
@@ -13,7 +13,8 @@ void GUI::StateSwitchButton(float btnPosX, bool *statePtr, const char *title) {
   }
 }
 
-void GUI::ControlRange(const char* title, float *valuePtr, float minValue, float maxValue) {
+void GUI::ControlRange(const char *title, float *valuePtr, float minValue,
+                       float maxValue) {
   GuiSliderBar((Rectangle){70, 20, 100, 20}, title,
                TextFormat("%.2f", *valuePtr), valuePtr, minValue, maxValue);
 }
